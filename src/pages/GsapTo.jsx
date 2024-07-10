ac1b0a9d696b654da320a6314a1039c32b882d2b
Kaustubh <kaustubhkumbhare02@gmail.com>
2024-07-10 18:36:43 +0530

    gsap-to

--- src/pages/GsapTo.jsx
+++ src/pages/GsapTo.jsx
@@ -1,5 +1,20 @@
+import { useGSAP } from '@gsap/react'
+import gsap from 'gsap'
+
+
+
 const GsapTo = () => {
-  // TODO: Implement the gsap.to() method
+
+  useGSAP(() => {
+    gsap.to('#blue-box', {
+      x: "50vw",
+      rotate: 360,
+      repeat: -1,
+      yoyo: true,
+      duration:2,
+      ease:'circ.in'
+    })
+  }, [])
 
   return (
     <main>
